eacfcc465d33015297b4f9670b56257f0c4b0975
William Souza <williamsouza@MacBook-Air-de-William.local>
2026-04-13 11:21:48 -0300

    fix supabase client

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://fkzpdcekozcncsnwyblp.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrenBkY2Vrb3pjbmNzbnd5YmxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2OTcyMjIsImV4cCI6MjA5MTI3MzIyMn0.X3ynnsjMJMEEVC1ZHUuqVJ_I5sooCkt1H_JoP0VtrUE";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
